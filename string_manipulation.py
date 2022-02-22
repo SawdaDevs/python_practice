e0e9b23b2d40f9f805ca1ff1dcf0fed06129121d
SawdaDevs <sawda.onanuga@gmail.com>
2022-02-22 09:30:28 +0000

    used replace method on simple strings, to comment and continue

---
+++ string_manipulation.py
@@ -0,0 +1,14 @@
+print('This is string manipulation practice!')
+
+init_string = 'Hello my name is Sally'
+init_string = init_string.replace('Sally', 'Sawda')
+
+print(init_string)
+
+second_string = "Hello World World World"
+second_string = second_string.replace("World","Universe")
+print(second_string)
+
+third_string = "Hello World World World"
+third_string = third_string.replace("World","Universe", 1)
+print(third_string)
